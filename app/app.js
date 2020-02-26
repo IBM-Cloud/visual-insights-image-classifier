50f822a8635910c01783e7b11bbf5789bbd2261c
Vidyasagar Machupalli <vidyasagarmsc@gmail.com>
2020-02-26 11:45:36 +0530

    Added error handling

--- app/app.js
+++ app/app.js
@@ -1,54 +1,60 @@
-'use strict';
-const express = require('express')
-const request = require('request');
-const app = express()
-const path = require('path')
-require('dotenv').config({
-  silent: true,
+"use strict";
+const express = require("express");
+const request = require("request");
+const app = express();
+const path = require("path");
+require("dotenv").config({
+  silent: true
 });
 
-const port = 3000
+const port = 3000;
 const MISSING_ENV =
-  'Missing required runtime environment variable POWERAI_VISION_API_URL';
+  "Missing required runtime environment variable POWERAI_VISION_API_URL";
 
 const poweraiVisionApiUrl = process.env.POWERAI_VISION_API_URL;
 
-console.log('Web API URL: ' + poweraiVisionApiUrl);
+console.log("Web API URL: " + poweraiVisionApiUrl);
 
 if (!poweraiVisionApiUrl) {
   console.log(MISSING_ENV);
 }
 
-app.use(express.static('public'))
+app.use(express.static("public"));
 /*
-* Default route for the web app
-*/
-app.get('/', (req, res) => {
-    res.sendFile('index.html', {
-        root: path.join(__dirname, './')
-    })
-})
+ * Default route for the web app
+ */
+app.get("/", (req, res) => {
+  res.sendFile("index.html", {
+    root: path.join(__dirname, "./")
+  });
+});
 /*
-* Upload an image for classification
-*/
-app.post('/uploadimage', function(req, res) {
+ * Upload an image for classification
+ */
+app.post("/uploadimage", function(req, res) {
   if (!poweraiVisionApiUrl) {
     console.log(MISSING_ENV);
-    res.send({data: JSON.stringify({error: MISSING_ENV})});
+    res.send({ data: JSON.stringify({ error: MISSING_ENV }) });
   } else {
-    req.pipe(request.post({
-      url: poweraiVisionApiUrl,
-      gzip: true,
-      agentOptions: {
-        rejectUnauthorized: false,
-      }}, function(err, resp, body) {
-      if (err) {
-        console.log(err);
-      }
-      console.log(body);
-      res.send({data: body});
-    }));
+    req.pipe(
+      request.post(
+        {
+          url: poweraiVisionApiUrl,
+          gzip: true,
+          agentOptions: {
+            rejectUnauthorized: false
+          }
+        },
+        function(err, resp, body) {
+          if (err) {
+            console.log(err);
+          }
+          console.log(body);
+          res.send({ data: body });
+        }
+      )
+    );
   }
 });
 
-app.listen(port, () => console.log(`App listening on port ${port}!`))
+app.listen(port, () => console.log(`App listening on port ${port}!`));
--- app/public/index.js
+++ app/public/index.js
@@ -1,94 +1,107 @@
 $(document).ready(function() {
-    $("#classifybtn").attr("disabled", true);
-     $("#file").change(function () {
+  $("p.error").text("");
+  $("#classifybtn").attr("disabled", true);
+  $("#file").change(function() {
     showUploadedImage(this);
-});
-     $('#imageForm').submit(function() {
-      $("#classifybtn").addClass("is-loading");
-      $("#table tbody tr").remove(); 
-        $(this).ajaxSubmit({
-            error: function(xhr) {
-        status('Error: ' + xhr.status);
-            },
-            success: function(response) {
-                $("#classifybtn").removeClass("is-loading");
-                $("#classifybtn").attr("disabled", true);
-                 console.log(response);  
-                 var parsedJSON = JSON.parse(response.data);
-                 console.log(parsedJSON.classified);
-                 var classifiedCategory = Object.keys(parsedJSON.classified)[0] == "_negative_" ? "Uncategorized" : Object.keys(parsedJSON.classified)[0]; 
-                 var confidence = parseFloat(Object.values(parsedJSON.classified)[0]);
-                 $("#table tbody").append("<tr><td>"+classifiedCategory+"</td><td>"+(confidence*100).toFixed(2)+" %</td></tr>");
-                 $("#modal").addClass("is-active");
-                 $('#modal-close').on('click', function() {
-                   $("#modal").removeClass("is-active");
+  });
+  $("#imageForm").submit(function() {
+    $("#classifybtn").addClass("is-loading");
+    $("#table tbody tr").remove();
+    $(this).ajaxSubmit({
+      error: function(xhr) {
+        $("p.error").text(xhr.status);
+      },
+      success: function(response) {
+        $("#classifybtn").removeClass("is-loading");
+        $("#classifybtn").attr("disabled", true);
+        console.log(response);
+        var parsedJSON = JSON.parse(response.data);
+        if (parsedJSON.result == "success") {
+          console.log(parsedJSON.classified);
+          var classifiedCategory =
+            Object.keys(parsedJSON.classified)[0] == "_negative_"
+              ? "Uncategorized"
+              : Object.keys(parsedJSON.classified)[0];
+          var confidence = parseFloat(Object.values(parsedJSON.classified)[0]);
+          $("#table tbody").append(
+            "<tr><td>" +
+              classifiedCategory +
+              "</td><td>" +
+              (confidence * 100).toFixed(2) +
+              " %</td></tr>"
+          );
+          $("#modal").addClass("is-active");
+          $("#modal-close").on("click", function() {
+            $("#modal").removeClass("is-active");
+          });
+        } else if (parsedJSON.result == "fail") {
+          $("p.error").text(parsedJSON.fault);
+        } else {
+          $("p.error").text(parsedJSON.error);
+        }
+      }
     });
-            }
-    });
-        //Very important line, it disable the page refresh.
     return false;
-    });    
+  });
 });
 
-//TODO: Remove 
+//TODO: Remove
 var classifyBtn = document.getElementById("classifybtn");
 function classifyImageWithTensorflow() {
-    const img = document.getElementById('uploadedimage');
-    var modalDlg = document.getElementById('modal');
-    var table = document.getElementById('table').getElementsByTagName('tbody')[0];
-    classifyBtn.classList.add("is-loading");
-    // Load the model.
-    mobilenet.load().then(model => {
-        // Classify the image.
-        model.classify(img).then(predictions => {
-            console.log('Predictions: ');
-            console.log(predictions);
-            table.innerHTML="";
-            for (var i = 0; i < predictions.length; i++) {
-                var tr = document.createElement('tr');
-                var category = predictions[i]["className"];
-                var confidence = predictions[i]["probability"].toFixed(5);
-                var tdCategory = tr.appendChild(document.createElement('td'));
-                tdCategory.innerHTML = category;
-                var tdConfidence = tr.appendChild(document.createElement('td'));
-                tdConfidence.innerHTML = confidence;
-                table.appendChild(tr);
-            }
-            modalDlg.classList.add('is-active');
-            classifyBtn.classList.remove("is-loading");
-        });
+  const img = document.getElementById("uploadedimage");
+  var modalDlg = document.getElementById("modal");
+  var table = document.getElementById("table").getElementsByTagName("tbody")[0];
+  classifyBtn.classList.add("is-loading");
+  // Load the model.
+  mobilenet.load().then(model => {
+    // Classify the image.
+    model.classify(img).then(predictions => {
+      console.log("Predictions: ");
+      console.log(predictions);
+      table.innerHTML = "";
+      for (var i = 0; i < predictions.length; i++) {
+        var tr = document.createElement("tr");
+        var category = predictions[i]["className"];
+        var confidence = predictions[i]["probability"].toFixed(5);
+        var tdCategory = tr.appendChild(document.createElement("td"));
+        tdCategory.innerHTML = category;
+        var tdConfidence = tr.appendChild(document.createElement("td"));
+        tdConfidence.innerHTML = confidence;
+        table.appendChild(tr);
+      }
+      modalDlg.classList.add("is-active");
+      classifyBtn.classList.remove("is-loading");
     });
-    classifyBtn.setAttribute("disabled", "");
-    modal.removeAttribute("is-active");
-    var imageModalCloseBtn = document.querySelector('#modal-close');
-    imageModalCloseBtn.addEventListener('click', function() {
-        modalDlg.classList.remove('is-active');
-    });
+  });
+  classifyBtn.setAttribute("disabled", "");
+  modal.removeAttribute("is-active");
+  var imageModalCloseBtn = document.querySelector("#modal-close");
+  imageModalCloseBtn.addEventListener("click", function() {
+    modalDlg.classList.remove("is-active");
+  });
 }
 
 function showUploadedImage(fileInput) {
-    var files = fileInput.files;
-    if (files.length > 0) {
-        for (var i = 0; i < files.length; i++) {
-            var file = files[i];
-            var imageType = /image.*/;
-            if (!file.type.match(imageType)) {
-                continue;
-            }
-            const fileName = document.querySelector('#file-js-example .file-name');
-            fileName.textContent = files[0].name;
-            var img = document.getElementById("uploadedimage");
-            img.file = file;
-            var reader = new FileReader();
-            reader.onload = (function(aImg) {
-                return function(e) {
-                    aImg.src = e.target.result;
-                    classifyBtn.removeAttribute("disabled");
-                };
-            })(img);
-            reader.readAsDataURL(file);
-        }
+  var files = fileInput.files;
+  if (files.length > 0) {
+    for (var i = 0; i < files.length; i++) {
+      var file = files[i];
+      var imageType = /image.*/;
+      if (!file.type.match(imageType)) {
+        continue;
+      }
+      const fileName = document.querySelector("#file-js-example .file-name");
+      fileName.textContent = files[0].name;
+      var img = document.getElementById("uploadedimage");
+      img.file = file;
+      var reader = new FileReader();
+      reader.onload = (function(aImg) {
+        return function(e) {
+          aImg.src = e.target.result;
+          classifyBtn.removeAttribute("disabled");
+        };
+      })(img);
+      reader.readAsDataURL(file);
     }
+  }
 }
-
-  
