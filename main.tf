5f2a7da31c3ccbe16ffec05b5ee01db7aec89591
Vidyasagar Machupalli <vidyasagarmsc@gmail.com>
2020-03-03 11:30:50 +0530

    tf 0.11 scripts

--- main.tf
+++ main.tf
@@ -1,38 +1,38 @@
 provider "ibm" {
-  region           = var.region
-  ibmcloud_api_key = var.ibmcloud_api_key
-  generation       = var.generation
-  ibmcloud_timeout = var.ibmcloud_timeout
+  region           = "${var.region}"
+  ibmcloud_api_key = "${var.ibmcloud_api_key}"
+  generation       = "${var.generation}"
+  ibmcloud_timeout = "${var.ibmcloud_timeout}"
 }
 
 data "ibm_resource_group" "group" {
-  name = var.resource_group_name
+  name = "${var.resource_group_name}"
 }
 
 resource "ibm_is_security_group" "frontend_sg" {
   name           = "${var.basename}-frontend"
-  vpc            = var.vpc_id
-  resource_group = data.ibm_resource_group.group.id
+  vpc            = "${"${var.vpc_id}"}"
+  resource_group = "${data.ibm_resource_group.group.id}"
 }
 
 # allow ssh access to this instance from outside
 resource "ibm_is_security_group_rule" "ingress_ssh_all" {
-  group     = ibm_is_security_group.frontend_sg.id
+  group     = "${ibm_is_security_group.frontend_sg.id}"
   direction = "inbound"
   remote    = "0.0.0.0/0" # TOO OPEN for production
 
-  tcp {
+  tcp =  {
     port_min = 22
     port_max = 22
   }
 }
 
 resource "ibm_is_security_group_rule" "frontend_ingress_80_all" {
-  group     = ibm_is_security_group.frontend_sg.id
+  group     = "${ibm_is_security_group.frontend_sg.id}"
   direction = "inbound"
   remote    = "0.0.0.0/0"
 
-  tcp {
+  tcp =  {
     port_min = 80
     port_max = 80
   }
@@ -40,11 +40,11 @@ resource "ibm_is_security_group_rule" "frontend_ingress_80_all" {
 
 // For node app
 resource "ibm_is_security_group_rule" "frontend_ingress_3000_node" {
-  group     = ibm_is_security_group.frontend_sg.id
+  group     = "${ibm_is_security_group.frontend_sg.id}"
   direction = "inbound"
   remote    = "0.0.0.0/0"
 
-  tcp {
+  tcp =  {
     port_min = 3000
     port_max = 3000
   }
@@ -52,40 +52,40 @@ resource "ibm_is_security_group_rule" "frontend_ingress_3000_node" {
 
 
 resource "ibm_is_security_group_rule" "maintenance_egress_443" {
-  group     = ibm_is_security_group.frontend_sg.id
+  group     = "${ibm_is_security_group.frontend_sg.id}"
   direction = "outbound"
   remote    = "0.0.0.0/0"
 
-  tcp {
+  tcp =  {
     port_min = "443"
     port_max = "443"
   }
 }
 
 resource "ibm_is_security_group_rule" "maintenance_egress_80" {
-  group     = ibm_is_security_group.frontend_sg.id
+  group     = "${ibm_is_security_group.frontend_sg.id}"
   direction = "outbound"
   remote    = "0.0.0.0/0"
 
-  tcp {
+  tcp =  {
     port_min = 80
     port_max = 80
   }
 }
 
 resource "ibm_is_security_group_rule" "maintenance_egress_53" {
-  group     = ibm_is_security_group.frontend_sg.id
+  group     = "${ibm_is_security_group.frontend_sg.id}"
   direction = "outbound"
   remote    = "0.0.0.0/0"
 
-  tcp {
+  tcp =  {
     port_min = 53
     port_max = 53
   }
 }
 
 resource "ibm_is_security_group_rule" "maintenance_egress_udp_53" {
-  group     = ibm_is_security_group.frontend_sg.id
+  group     = "${ibm_is_security_group.frontend_sg.id}"
   direction = "outbound"
   remote    = "0.0.0.0/0"
 
@@ -97,13 +97,13 @@ resource "ibm_is_security_group_rule" "maintenance_egress_udp_53" {
 
 resource "ibm_is_subnet" "frontend_subnet" {
   name                     = "${var.basename}-frontend-subnet"
-  vpc                      = var.vpc_id
-  zone                     = var.zone
+  vpc                      = "${var.vpc_id}"
+  zone                     = "${var.zone}"
   total_ipv4_address_count = 256
 }
 
 data "ibm_is_ssh_key" "ssh_key" {
-  name = var.ssh_key_name
+  name = "${var.ssh_key_name}"
 }
 
 data "ibm_is_image" "ubuntu" {
@@ -112,31 +112,30 @@ data "ibm_is_image" "ubuntu" {
 
 resource "ibm_is_instance" "frontend_vsi" {
   name           = "${var.basename}-frontend-vsi"
-  vpc            = var.vpc_id
-  zone           = var.zone
-  keys           = [data.ibm_is_ssh_key.ssh_key.id]
-  image          = data.ibm_is_image.ubuntu.id
+  vpc            = "${var.vpc_id}"
+  zone           = "${var.zone}"
+  keys           = ["${data.ibm_is_ssh_key.ssh_key.id}"]
+  image          = "${data.ibm_is_image.admin_image_name.id}"
   profile        = "cx2-2x4"
-  resource_group = data.ibm_resource_group.group.id
+  resource_group = "${data.ibm_resource_group.group.id}"
 
   primary_network_interface {
-    subnet          = ibm_is_subnet.frontend_subnet.id
-    security_groups = [ibm_is_security_group.frontend_sg.id]
+    subnet          = "${element(ibm_is_subnet.sub_admin.id, count.index)}"
+    security_groups = ["${ibm_is_security_group.frontend_sg.id}"]
   }
 }
 
 resource "ibm_is_floating_ip" "frontend_fip" {
   name   = "${var.basename}-frontend-fip"
-  target = ibm_is_instance.frontend_vsi.primary_network_interface[0].id
-}
+  target = "${ibm_is_instance.vpc_vsi_admin.primary_network_interface.0.id}"
 
 resource "null_resource" "provisioners" {
   connection {
     type    = "ssh"
     user    = "root"
     timeout = "2m"
-    host    = ibm_is_floating_ip.frontend_fip.address
-    private_key = file("${var.ssh_private_key_file_path}")
+    host    = "${ibm_is_floating_ip.frontend_fip.0.address}"
+    private_key = "${file("${var.ssh_private_key_file_path}")}"
   }
 
   provisioner "file" {
